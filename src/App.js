e7ea5ac11367abfabc518f855e2123963b4e52a7
MhdRahman <iamrahmanansari@tutanota.com>
2026-01-01 15:20:45 +0530

    Onboarded Analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [email, setEmail] = useState('');
@@ -332,6 +333,7 @@ function App() {
           </div>
         </div>
       </footer>
+      <Analytics />
     </div>
   );
 }
